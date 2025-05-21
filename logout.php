6a5e486954decb1bdb423f6a40d8f192b101312c
mst.fahimajjaman jaina <0562310005101045@neub.edu.bd>
2025-05-21 08:17:28 +0600

    code for logout in employee changed and make it like elms

--- logout.php
+++ logout.php
@@ -1,33 +1,15 @@
 <?php
-session_start();
-
-// Clear all session variables
+session_start(); 
 $_SESSION = array();
-
-// Clear session cookie
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 3600,
+    setcookie(session_name(), '', time() - 60*60,
         $params["path"], $params["domain"],
         $params["secure"], $params["httponly"]
     );
 }
-
-// Unset specific session variables
-unset($_SESSION['eid']);
-unset($_SESSION['name']);
-unset($_SESSION['emailId']);
-
-// Destroy the session
-session_destroy();
-
-// Prevent caching of this page
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-
-// Redirect to login page
-header("Location: index.php");
-exit();
+unset($_SESSION['alogin']);
+session_destroy(); // destroy session
+header("location:index.php"); 
 ?>
 
